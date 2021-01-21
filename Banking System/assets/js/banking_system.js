8d613a32f5206d2812395513cbc57fa084d8061f
natneam <se.natneam.mesele@gmail.com>
2021-01-21 11:27:07 +0300

    Banking system app finished

---
+++ Banking System/assets/js/banking_system.js
@@ -0,0 +1,57 @@
+// Banking system
+var accounts = [100, 346, 789, 435]
+
+function deposit(amount, accNum){
+    accounts[accNum] += amount
+    console.log("Deposited to : " + accNum + " Deposited Amount: " + amount + " total balance : " + accounts[accNum])
+    return true
+}
+
+function withdraw(amount, accNum){
+    if (amount < accounts[accNum]){
+        if(amount < 100){
+            accounts[accNum] -= amount
+            console.log("Withdrawn from : "+ accNum + " Withdrawn Amount: " + amount + " total balance : " + accounts[accNum])
+            return true
+        }else{
+            alert("you cannot withdraw more than $100")
+        }
+    } else{
+        alert("No enough Money to withdraw")
+    }
+    return false
+}
+
+function balance(accNum){
+    alert("Your Balance is " + accounts[accNum])
+}
+
+function transfer(from, to, amount){
+    if (withdraw(amount, from)){
+        deposit(amount, to)
+    }
+}
+
+(
+    function(){
+        service = parseInt(prompt("What do you want to do today: \n 1: Deposit \n 2: Withdraw \n 3: Inquire Balance \n 4: transfer"))
+        console.log(accounts)
+        switch (service) {
+            case 1:
+                deposit(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
+                break;
+            case 2:
+                withdraw(parseInt(prompt("Enter amount: ")), parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
+                break;
+            case 3:
+                balance(parseInt(prompt("Enter account Number(between 0 and "+(accounts.length-1)+"): ")));
+                break;
+            case 4:
+                transfer(parseInt(prompt("Enter account to debt from Number(between 0 and "+(accounts.length-1)+"): ")),parseInt(prompt("Enter account to deposit into Number(between 0 and "+(accounts.length-1)+"): ")), parseInt(prompt("Enter amount: ")));
+                break;
+            default:
+                alert("Invalid Input");
+                break;
+        }
+    }
+)();
